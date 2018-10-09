27f6aabb60b0a060763e5ce98be2ef8580d6e3c5
Kaya Bax <kayawsm@hotmail.com>
2018-10-09 12:36:34 +0200

    Start defining the used class

--- ProgBeatGen.py
+++ ProgBeatGen.py
@@ -20,9 +20,42 @@
 #
 # -----stappenplan----- #
 # 1. Defineer een class, met alle opties in __init__ die je wilt aanroepen (bpm, random/vast ritme [y/n], gegeven ritme, ritme>midi [y/n], lijst met samples, play, pause)
-# 2. defineer functies voor het afspelen van de sample, met nog te definieren variabelen
+# 2. defineer zowel een functie die elke maat van maatsoort kan veranderen, evenals een functie met een aan te geven vaste maatsoort
 #    definieer ook debug functies die print of je de juiste variabelen hebt.
 # 3. vraag de gebruiker om alle variabelen.
 # 4. Roep de juiste functie aan om de samples af te spelen met de juiste variabelen.
+# 5. Debugging
 # 5. Zorg voor een stop functie:
-# 6. hoi
+
+
+class userInput:
+    def __init__(self, bpm, low, mid, high, choice, sig, exp):
+        self.bpm = bpm
+        self.low = low
+        self.mid = mid
+        self.high = high
+        self.choice = choice
+        self.sig = sig
+        self.exp = exp
+
+    def randomSeq(self):
+        print() #placeholder
+
+    def staticSeq(self):
+        print() #placeholder
+
+    def debug(self):
+        print("The BPM is: " + str(self.bpm))
+        print("The low sample is: " + self.low)
+        print("The mid sample is: " + self.mid)
+        print("The high sample is: " + self.high)
+        if self.choice == True:
+            print ("You've chosen to have a changing signature.")
+        elif self.choice == False:
+            print ("You've chosen to have a static signature.")
+        print("The signature you've chosen is: " + self.sig)
+        print("the file will be exported as: " + self.exp)
+
+p1 = userInput(120, "low.wav", "mid.wav", "high.wav", False, "5/8", "Export.midi")
+
+p1.debug()
