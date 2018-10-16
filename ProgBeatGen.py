d2ff0dac81ed8ed7d843be9cc3a1bf8e3393fc78
Kaya Bax <kayawsm@hotmail.com>
2018-10-16 10:30:41 +0200

    All variables asked and added. TODO: make sequencers; research MIDI

--- ProgBeatGen.py
+++ ProgBeatGen.py
@@ -26,9 +26,10 @@
 # 4. Roep de juiste functie aan om de samples af te spelen met de juiste variabelen.
 # 5. Debugging
 # 5. Zorg voor een stop functie:
-<<<<<<< HEAD
-
-
+# ---------- Imports ----------#
+import simpleaudio as sa
+import time, os, fnmatch, sys, random
+# ---------- Classes ---------- #
 class userInput:
     def __init__(self, bpm, low, mid, high, choice, sig, exp):
         self.bpm = bpm
@@ -41,26 +42,99 @@ class userInput:
 
     def randomSeq(self):
         print() #placeholder
+        # TODO: Make an array with notes
+
 
     def staticSeq(self):
         print() #placeholder
 
     def debug(self):
-        print("The BPM is: " + str(self.bpm))
-        print("The low sample is: " + self.low)
-        print("The mid sample is: " + self.mid)
-        print("The high sample is: " + self.high)
+        print("The BPM is: %d" % (self.bpm))
+        print("The low sample is: %s" % (self.low))
+        print("The mid sample is: %s" % (self.mid))
+        print("The high sample is: %s" % (self.high))
         if self.choice == True:
             print ("You've chosen to have a changing signature.")
         elif self.choice == False:
             print ("You've chosen to have a static signature.")
-        print("The signature you've chosen is: " + self.sig)
-        print("the file will be exported as: " + self.exp)
+        print("The signature you've chosen is: %s/16" % (self.sig))
+        print("the file will be exported as: %s" % (self.exp))
 
-p1 = userInput(120, "low.wav", "mid.wav", "high.wav", False, "5/8", "Export.midi")
+# ---------- Initialization ----------#
+audioFiles = []
+listOfFiles = os.listdir('./Samples')
+pattern = "*.wav"
+for entry in listOfFiles:
+    if fnmatch.fnmatch(entry, pattern):
+        audioFiles.append(entry)
+
+
+# ---------- Variables ----------#
+# set the bpm of the beat generator:
+bpm = 120
+while True:
+    try:
+        ask = input("The current BPM is %s beats per minute. would you like to change it? [Y/N] " %(bpm)).lower()
+        if ask == "y":
+            bpm = int(input("Please fill in what BPM you would like. "))
+            break
+        elif ask == "n":
+            break
+        else:
+            print("That's not a yes or a no. Please use Y or N as your answer.")
+            print()
+    except:
+        print() #safecheck
+
+# list all files, choose which samples go to the 'low', 'mid', and 'high' sampleslots:
+for file in audioFiles:
+    print(file)
+while True:
+    try:
+        low = input("What sample should the low slot be? ")
+        if not low.endswith('.wav'):
+            low+=".wav"
+        lowPlay = sa.WaveObject.from_wave_file("Samples/"+low)
+        break
+    except FileNotFoundError:
+        print("ERROR: There's no such file in the directory. please use one of the filenames from the list above.")
+        print()
+while True:
+    try:
+        mid = input("What sample should the mid slot be? ")
+        if not mid.endswith('.wav'):
+            mid+=".wav"
+        midPlay = sa.WaveObject.from_wave_file("Samples/"+mid)
+        break
+    except FileNotFoundError:
+        print("ERROR: There's no such file in the directory. please use one of the filenames from the list above.")
+        print()
+while True:
+    try:
+        high = input("What sample should the high slot be? ")
+        if not high.endswith('.wav'):
+            high+=".wav"
+        highPlay = sa.WaveObject.from_wave_file("Samples/"+high)
+        break
+    except FileNotFoundError:
+        print("ERROR: There's no such file in the directory. please use one of the filenames from the list above.")
+        print()
+
+while True:
+    choice = input("do you want a static or a random signature? ").lower()
+    if choice == "static":
+        sig = input("Please fill in how many 16th notes you want as your signature.")
+        break
+    elif choice == "random":
+        sig = random.randint(11, 32)
+        break
+    else:
+        print("That's not a yes or a no. Please use Y or N as your answer.")
+        print()
+
+exp = "poep.midi"
+p1 = userInput(bpm, low, mid, high, choice, sig, exp)
+
+
 
 p1.debug()
-=======
-# 6. hoi
-print("JA JE MOEDERJONHUH")
->>>>>>> 7823367012ab06c285bd0732a5f2f7e7116d2f51
